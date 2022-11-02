58852304a31ff98b5d393fe3756ede5bc72ae781
Gabe Demanche <gldemanche@wpi.edu>
2022-11-02 15:33:22 -0400

    added model and puzzle .js files

---
+++ src/model/Puzzle.js
@@ -0,0 +1,117 @@
+export const puzzleInformation = {
+  name: "NinjaSe 4x5",
+  board: {
+    rows: "5",
+    columns: "4",
+  },
+  doors: [
+    {
+      label: "A",
+      isLocked: "true",
+      keyColor: "green",
+    },
+    {
+      label: "B",
+      isLocked: "true",
+      keyColor: "red",
+    },
+  ],
+  walls: [
+    {
+      label: "C",
+    },
+    {
+      label: "D",
+    },
+    {
+      label: "E",
+    },
+    {
+      label: "F",
+    },
+    {
+      label: "G",
+    },
+    {
+      label: "H",
+    },
+    {
+      label: "I",
+    },
+    {
+      label: "J",
+    },
+  ],
+  locations: [
+    {
+      door: "A",
+      location: {
+        row: "3",
+        column: "4",
+      },
+    },
+    {
+      door: "B",
+      location: {
+        row: "5",
+        column: "2",
+      },
+    },
+    {
+      wall: "C",
+      location: {
+        row: "1",
+        column: "4",
+      },
+    },
+    {
+      wall: "D",
+      location: {
+        row: "2",
+        column: "2",
+      },
+    },
+    {
+      wall: "E",
+      location: {
+        row: "2",
+        column: "3",
+      },
+    },
+    {
+      wall: "F",
+      location: {
+        row: "2",
+        column: "4",
+      },
+    },
+    {
+      wall: "G",
+      location: {
+        row: "4",
+        column: "1",
+      },
+    },
+    {
+      wall: "H",
+      location: {
+        row: "4",
+        column: "2",
+      },
+    },
+    {
+      wall: "I",
+      location: {
+        row: "4",
+        column: "4",
+      },
+    },
+    {
+      wall: "J",
+      location: {
+        row: "5",
+        column: "4",
+      },
+    },
+  ],
+};
